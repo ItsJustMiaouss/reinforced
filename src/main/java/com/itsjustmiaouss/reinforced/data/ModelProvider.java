f6bb96d55dc8e42cec4c645de4b3ba7e4e8e6e63
ItsJustMiaouss <59478524+ItsJustMiaouss@users.noreply.github.com>
2024-04-02 03:54:46 +0200

    feat: simplify model provider

--- src/main/java/com/itsjustmiaouss/reinforced/data/ModelProvider.java
+++ src/main/java/com/itsjustmiaouss/reinforced/data/ModelProvider.java
@@ -1,17 +1,12 @@
 package com.itsjustmiaouss.reinforced.data;
 
-import com.google.gson.JsonElement;
 import com.itsjustmiaouss.reinforced.block.AbstractReinforcedBlock;
 import com.itsjustmiaouss.reinforced.block.ReinforcedBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.block.Block;
-import net.minecraft.data.client.*;
-import net.minecraft.util.Identifier;
-
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.TexturedModel;
 
 class ModelProvider extends FabricModelProvider {
 
@@ -19,21 +14,12 @@ class ModelProvider extends FabricModelProvider {
         super(output);
     }
 
-    private void registerSidedBlock(Consumer<BlockStateSupplier> blockStateCollector, BiConsumer<Identifier, Supplier<JsonElement>> modelCollector, Block block) {
-        TextureMap textureMap = new TextureMap().put(TextureKey.BOTTOM, TextureMap.getSubId(block, "_bottom")).put(TextureKey.TOP, TextureMap.getSubId(block, "_top")).put(TextureKey.SIDE, TextureMap.getSubId(block, "_side"));
-        blockStateCollector.accept(BlockStateModelGenerator.createSingletonBlockState(block, Models.CUBE_BOTTOM_TOP.upload(block, textureMap, modelCollector)));
-    }
-
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
         ReinforcedBlocks.getReinforcedBlocks().stream()
                 .filter(filteredBlock -> filteredBlock instanceof AbstractReinforcedBlock)
                 .map(filteredBlock -> (AbstractReinforcedBlock) filteredBlock)
-                .forEach(abstractReinforcedBlock -> registerSidedBlock(
-                        blockStateModelGenerator.blockStateCollector,
-                        blockStateModelGenerator.modelCollector,
-                        abstractReinforcedBlock
-                ));
+                .forEach(abstractReinforcedBlock -> blockStateModelGenerator.registerSingleton(abstractReinforcedBlock, TexturedModel.CUBE_BOTTOM_TOP));
     }
 
     @Override
